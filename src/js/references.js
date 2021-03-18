ce408f1178b718d958f3ad4196465611c6297833
capricornus44 <antonzavalniuk@gmail.com>
2021-03-18 17:06:51 +0100

    Create references.js

---
+++ src/js/references.js
@@ -0,0 +1,8 @@
+export default {
+  searchForm: document.querySelector('#search-form'),
+  search: document.querySelector('.search'),
+  gallery: document.querySelector('.gallery'),
+  scroll: document.querySelector('.page-load-status'),
+  themeSwitchToggle: document.querySelector('#theme-switch-toggle'),
+  dotsBox: document.querySelectorAll('.dots-box'),
+};
