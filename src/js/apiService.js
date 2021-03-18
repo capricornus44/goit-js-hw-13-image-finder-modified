5d1cc8fc9b4ebe28235019161256dac1e8495eb7
capricornus44 <antonzavalniuk@gmail.com>
2021-03-18 17:07:57 +0100

    Create class ApiService

---
+++ src/js/apiService.js
@@ -0,0 +1,40 @@
+const API_KEY = '20669309-c97d1ec468a66ad87fd39e114';
+const BASE_URL = 'https://pixabay.com/api';
+
+export default class ApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  async fetchArticles() {
+    const url = `${BASE_URL}/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=20&key=${API_KEY}`;
+
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error('Request was rejected');
+    }
+
+    const { total, hits } = await response.json();
+    console.log(`Page: ${this.page}`, { total, hits });
+    this.incrementPage();
+    return { total, hits };
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
